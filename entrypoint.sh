ba8d833c4865c66f000d291181362802de8ce5d4
raldozamora <132487402+raldozamora@users.noreply.github.com>
2023-06-06 10:30:15 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,19 +14,19 @@ fi
 
 # create snapshot with file, store result in json
 FILE_S_CREATE="/action_work_folder/s_create.json"
-npx bluemachine -t ${TM_TOKEN_KEY} snapshot create --repo-id ${1} --file ${2} --json-output ${FILE_S_CREATE}
+npx bluemachinecli -t ${TM_TOKEN_KEY} snapshot create --repo-id ${1} --file ${2} --json-output ${FILE_S_CREATE}
 if test -f "$FILE_S_CREATE"; then
     # analyze snapshot
     SNAPSHOT_ID=$(jq '.ID' $FILE_S_CREATE)
     echo "Snapshot Id : $SNAPSHOT_ID"
     FILE_S_ANA="/action_work_folder/s_ana.json"
-    npx bluemachine -t ${TM_TOKEN_KEY} snapshot analyze --snapshot-id=${SNAPSHOT_ID} --json-output ${FILE_S_ANA}
+    npx bluemachinecli -t ${TM_TOKEN_KEY} snapshot analyze --snapshot-id=${SNAPSHOT_ID} --json-output ${FILE_S_ANA}
     if test -f "$FILE_S_ANA"; then
         # create report for analysis
         ANA_ID=$(jq '.[0].id' $FILE_S_ANA)
         echo "Analysis ID : $ANA_ID"
         FILE_A_R_OUTPUT="/action_work_folder/ana_r.json"
-        npx bluemachine -t ${TM_TOKEN_KEY} analyses report --analysis-id=${ANA_ID} --json-output ${FILE_A_R_OUTPUT}        
+        npx bluemachinecli -t ${TM_TOKEN_KEY} analyses report --analysis-id=${ANA_ID} --json-output ${FILE_A_R_OUTPUT}        
         if test -f "$FILE_A_R_OUTPUT"; then
             # get URL to download report file
             DOWNLOAD_URL=$(jq '.downloadURL' $FILE_A_R_OUTPUT)
