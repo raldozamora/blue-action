7596718e380f807c5befeb7d54c1dd52ac102ea6
HenryTong <taksantong@gmail.com>
2023-06-15 14:18:55 -0300

    test without api_URL

--- Dockerfile
+++ Dockerfile
@@ -7,10 +7,6 @@ ENV DEBIAN_FRONTEND=noninteractive LANG=C.UTF-8
 RUN apt-get update
 RUN apt-get install --no-install-recommends -y jq wget
 
-# folders for working files
-# RUN mkdir -p /action_work_folder
-# RUN mkdir -p /action_work_folder/result
-
 # Prepare entrypoint
 COPY entrypoint.sh /entrypoint.sh
 RUN chmod +x /entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -7,35 +7,31 @@ if [[ -z "${TM_TOKEN_KEY}" ]]; then
   exit 1
 fi
 
-if [[ -z "${TM_API_URL}" ]]; then
-  echo "Error: missing TM_API_URL env variable"
-  exit 1
-fi
 
-# create snapshot with file, store result in json
-FILE_S_CREATE="action_work_s_create.json"
-npx bluemachinecli -t ${TM_TOKEN_KEY} snapshot create --repo-id ${1} --file ${2} --json-output ${FILE_S_CREATE}
-if test -f "$FILE_S_CREATE"; then
-    # analyze snapshot
-    SNAPSHOT_ID=$(jq '.ID' $FILE_S_CREATE)
-    echo "Snapshot Id : $SNAPSHOT_ID"
-    FILE_S_ANA="action_work_s_ana.json"
-    npx bluemachinecli -t ${TM_TOKEN_KEY} snapshot analyze --snapshot-id=${SNAPSHOT_ID} --json-output ${FILE_S_ANA}
-    if test -f "$FILE_S_ANA"; then
-        # create report for analysis
-        ANA_ID=$(jq '.[0].id' $FILE_S_ANA)
-        echo "Analysis ID : $ANA_ID"
-        FILE_A_R_OUTPUT="action_work_ana_r.json"
-        npx bluemachinecli -t ${TM_TOKEN_KEY} analyses report --analysis-id=${ANA_ID} --json-output ${FILE_A_R_OUTPUT}        
-        if test -f "$FILE_A_R_OUTPUT"; then
-            # get URL to download report file
-            DOWNLOAD_URL=$(jq '.downloadURL' $FILE_A_R_OUTPUT)
+# overview of process: create a snapshot with the input source, analyze it, then download the result as a pdf report
+
+echo "Create a snapshot"
+COMMAND_OUTPUT=$(npx @testmachine.ai/cli -t ${TM_TOKEN_KEY} snapshot create --repo-id ${1} --file ${2} --output=json)
+
+# attempt parse json output from previous command, show error in console if failed to parse
+SNAPSHOT_ID=$(jq '.ID' <<< "$COMMAND_OUTPUT") || echo "Cannot parse output from command because: $COMMAND_OUTPUT"
+if [[ ! -z $SNAPSHOT_ID && $SNAPSHOT_ID != "null" ]]; then
+    echo "Created Snapshot Id for analysis: $SNAPSHOT_ID"
+    COMMAND_OUTPUT=$(npx @testmachine.ai/cli -t ${TM_TOKEN_KEY} snapshot analyze --snapshot-id=${SNAPSHOT_ID} --output=json)
+    # attempt parse json output from previous command, show error in console if failed to parse
+    ANA_ID=$(jq '.id' <<< "$COMMAND_OUTPUT")
+    if [[ ! -z $ANA_ID && $ANA_ID != "null" ]]; then
+        echo "Requesting report for generated Analysis Id: $ANA_ID"
+        COMMAND_OUTPUT=$(npx @testmachine.ai/cli -t ${TM_TOKEN_KEY} analyses report --analysis-id=${ANA_ID} --output=json)
+        # attempt parse json output from previous command, show error in console if failed to parse
+        DOWNLOAD_URL=$(jq '.downloadURLOfGeneratedReport' <<< "$COMMAND_OUTPUT")
+        if [[ ! -z $DOWNLOAD_URL && $DOWNLOAD_URL != "null" ]]; then
             # remove double quotes from beginning and end of download url
             temp="${DOWNLOAD_URL%\"}"
             DOWNLOAD_URL="${temp#\"}"
             # download file
-            echo "Downloading : $DOWNLOAD_URL"
+            echo "Downloading generated report PDF download URL: $DOWNLOAD_URL"
             wget -O action_work_result_report.pdf ${DOWNLOAD_URL}
-        fi   
+        fi
     fi
 fi
